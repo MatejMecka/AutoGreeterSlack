20240c4c9fc033031a6fe2dbebb8417ccf5298f9
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-11-28 20:18:59 +0100

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from slackclient import SlackClient
+import time
+import logging
+from config import *
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+handler = logging.FileHandler(log_file)
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+sc = SlackClient(slack_token)
+
+if sc.rtm_connect():
+    while True:
+        for slack_message in sc.rtm_read():
+            logger.info(slack_message)
+            message = slack_message.get("type")
+            user = slack_message.get("user")
+            if not message or not user:
+                continue
+            if message == "member_joined_channel":
+                sc.api_call("chat.postMessage",channel=user,as_user=True,text=messagetosend)
+
+            time.sleep(2)
+
+else:
+    logger.error("There is a problem connecting to Slack. Check your Internet connection or you might have problems with the API key.")
