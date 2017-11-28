41cfebd6039786395687f861b85db6eec4f3715c
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-11-28 20:37:59 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 log_file = "" # Fill this out for a logging
 slack_token = "" # token for slack bot
 welcomemessagedm = "" # what the bot should dm the person
-welcomemessagechannel = "" # what should the bot post on a channel
+welcomemessagechannel = "" # what should the bot post on a channel, leave empty to not post
 
