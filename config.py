33edcda37385be239cfc97ea2de9bef3a3837911
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-11-29 07:56:52 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-log_file = "" # Fill this out for a logging
+log_file = "autogreeter.log" # Fill this out for a logging. P.S It's a must!
 slack_token = "" # token for slack bot
 welcomemessagedm = "" # what the bot should dm the person
 welcomemessagechannel = "" # what should the bot post on a channel, leave empty to not post
